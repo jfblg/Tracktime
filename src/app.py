2745e6f4ebae692a2deb8c3c852e1a5305fa0ddb
Frank Janus <inbox.jfblg@gmail.com>
2017-02-08 07:29:28 +0100

    Add timedb model and stuff. Not able to capture and run app simultaneously

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask, render_template, session
+from src.models.timedb.timy import Timy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///timetrack.sqlite')
@@ -12,6 +13,10 @@ def create_table():
     ''' SQLAlchemy creates the tables it sees from the imports above.
     '''
     db.create_all()
+    db.session.commit()
+
+    dev = Timy()
+    dev.capture_start()
 
 @app.route('/')
 def home():
@@ -20,11 +25,13 @@ def home():
 from src.models.participants.views import participants_blueprint
 from src.models.categories.views import categories_blueprint
 from src.models.startlist.views import startlist_blueprint
+from src.models.timedb.views import timedb_blueprint
 # add another models
 
 app.register_blueprint(participants_blueprint, url_prefix="/participants")
 app.register_blueprint(categories_blueprint, url_prefix="/categories")
 app.register_blueprint(startlist_blueprint, url_prefix="/startlist")
+app.register_blueprint(timedb_blueprint, url_prefix="/timedb")
 # register another blueprints
 
 
@@ -32,3 +39,4 @@ if __name__ == "__main__":
     from src.common.database import db
     db.init_app(app)
     app.run(port=4999, debug = True)
+
